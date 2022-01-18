5b216295e640a60b75b7b7f6678730b2210f1d58
kstar1014 <washingtons1014@outlook.com>
2022-01-18 08:11:11 -0500

    update faq component

--- components/faq.tsx
+++ components/faq.tsx
@@ -92,6 +92,8 @@ export function FAQ() {
         {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 20 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.2 }} */}
         <motion.div
           initial={{ opacity: 0, y: 20 }}
